8421e7b78ef9ed34a80a7cdcd86f40d79d5511aa
Harry Sun <82039461+harrysun1240@users.noreply.github.com>
2026-08-04 18:09:19 +0800

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,7 @@
+import bisect
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        flattened = [num for row in matrix for num in row]
+        index = bisect.bisect_left(flattened, target)
+        return True if index < len(flattened) and flattened[index] == target else False
